803482418d09158c43a43e614b6ebc4847a8d07a
Levraiptitnouveau <mamialekipoy.dorian.dorian@gmail.com>
2023-06-05 10:30:03 +0200

    Fin version tel

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,29 @@
-console.info('Hello world');
+'use strict'
+let BurgerMenuButton = document.querySelector(".burgermenu_button");
+let BurgerMenu = document.querySelector(".nav_burger");
+let UpPage = document.querySelector(".uppage");
+let Body = document.querySelector("body");
+BurgerMenuButton.addEventListener("click", () => {
+
+    if (BurgerMenuButton.innerHTML == "X") {
+        BurgerMenu.classList.remove("actif");
+        Body.classList.remove("actif");
+        BurgerMenuButton.innerHTML = "ME</br>NU";
+    } else {
+        BurgerMenu.classList.add("actif");
+        Body.classList.add("actif");
+    BurgerMenuButton.innerHTML = "X";
+    }
+    
+})
+
+let Prologue = document.querySelectorAll("#link");
+Prologue[1].addEventListener("click", () => {
+        BurgerMenu.classList.remove("actif");
+        Body.classList.remove("actif");
+        BurgerMenuButton.innerHTML = "ME</br>NU";
+});
+
+UpPage.addEventListener("click", () => {
+    window.scrollTo(0, 0)
+})
